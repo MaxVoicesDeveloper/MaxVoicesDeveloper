e5567d5b4e37b836d5f641a3ec8913003016101c
MaxVoices <111234777+MaxVoicesDeveloper@users.noreply.github.com>
2023-11-27 04:21:56 +0300

    Адаптация + меню-бургер
    Сайт адаптирован под телефоны и добавлен меню-бургер

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-const input = document.querySelectorAll('')
+let burger = document.querySelector(".menu__burger");
+let checkburger = document.querySelector(".mobileMenu")
+let sob = document.querySelector(".sob");
+burger.addEventListener('click', function(){
+    checkburger.classList.toggle("checked");
+    burger.classList.toggle("active");
+});
+
+sob.addEventListener('click', function(){
+    checkburger.classList.toggle("checked");
+    burger.classList.toggle("active");
+});
+
+
